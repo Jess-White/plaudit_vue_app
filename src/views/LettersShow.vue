2cce76dfa5f89e6ac85b67dc4ed0266e2c71cf30
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-04-09 17:50:29 -0500

    added text to letters show page

--- src/views/LettersShow.vue
+++ src/views/LettersShow.vue
@@ -2,6 +2,18 @@
   <div class="letters-show">
 
      <div class="row">
+
+       <div class="col-md-6">
+        <h4 class="text-center">Title: {{letter.title}}</h4>
+       </div>
+
+       <div class="col-md-6">
+        <h4 class="text-center">User Name: {{user.name}}</h4>
+        <h4 class="text-center">User Name: {{user.name}}</h4>
+        <h4 class="text-center">User Name: {{user.name}}</h4>
+        <h4 class="text-center">User Name: {{user.name}}</h4>
+       </div>
+
        <div class="col-md-6">
         <h4 class="text-center">Name: {{recipient.first_name}} {{recipient.last_name}}</h4>
         <h4 class="text-center">Title: {{recipient.title}}</h4>
@@ -12,6 +24,30 @@
         <h4 class="text-center">Zip: {{recipient.zip}}</h4>
       </h4>
        </div>
+
+       <div class="col-md-6">
+        <h4 class="text-center">Letter Title: {{letter.title}}</h4>
+        <h4 class="text-center">Title: {{recipient.title}}</h4>
+        <h4 class="text-center">Email: {{recipient.email}}</h4>
+        <h4 class="text-center">Address: {{recipient.address}}</h4>
+        <h4 class="text-center">City: {{recipient.city}}</h4>
+        <h4 class="text-center">State: {{recipient.state}}</h4>
+        <h4 class="text-center">Zip: {{recipient.zip}}</h4>
+      </h4>
+       </div>
+
+        <div class="col-md-6">
+         <h4 class="text-center">Name: {{recipient.first_name}} {{recipient.last_name}}</h4>
+         <h4 class="text-center">Title: {{recipient.title}}</h4>
+         <h4 class="text-center">Email: {{recipient.email}}</h4>
+         <h4 class="text-center">Address: {{recipient.address}}</h4>
+         <h4 class="text-center">City: {{recipient.city}}</h4>
+         <h4 class="text-center">State: {{recipient.state}}</h4>
+         <h4 class="text-center">Zip: {{recipient.zip}}</h4>
+       </h4>
+        </div>
+
+
        <div>
           <router-link class="btn btn-info m-2" v-bind:to="'/recipients/' + recipient.id + '/edit'">Edit</router-link>
           <button class="btn btn-info m-2" v-on:click="destroyRecipient()">Delete</button>
@@ -68,33 +104,40 @@ var axios = require('axios');
         errors: []
       };
     },
-    created: function() {
-      axios 
-        .get("/api/recipients/" + this.$route.params.id)
-        .get("/api/referees/" + this.$route.params.id)
-        .get("/api/users/" + this.$route.params.id)
-        .then(response => {
-          this.recipient = response.data
-        });
+  created: function() {
+    axios 
+      .get("/api/recipients/" + this.$route.params.id)
+      .get("/api/referees/" + this.$route.params.id)
+      .get("/api/users/" + this.$route.params.id)
+      .then(response => {
+        this.recipient = response.data
+      });
     },
-    methods: {
-      destroyLetter: function() {
-        axios
-          .delete("/api/letters/" + this.$route.params.id)
-          .then(response => {
-            this.$router.push("/letters");
-          });
-      }
+methods: {
+  destroyLetter: function() {
+    axios.
+      .delete("/api/letters" this.$route.params.id)
+      .then(response => {
+        this.$router.push("/");
+      });
     },
-    watch: {
-      $route: function() {
-        axios
-        .get("/api/letters/" + this.$route.params.id)
-        .then(response => {
-          this.letters = response.data;
-        });
-      }  
+  printableLetter: function() {
+    axios.
+      .get("/api/letters/" + this.$route.params.id)
+      .then(response => {
+        this.$router.push("/letters/" + this$route.params.id + "/printable");
+      });
     }
+  },
+watch: {
+  $route: function() {
+    axios
+    .get("/api/letters/" + this.$route.params.id)
+    .then(response => {
+      this.letter = response.data;
+    });
+  }
+}
 };
 </script>
 
@@ -114,3 +157,31 @@ axios.all([getUserData(), getRecipientData(), getRefereeData()])
   this.recipients = response.data;
   this.referees = response.data;
 }))
+
+methods: {
+  destroyLetter: function() {
+    axios.
+      .delete("/api/letters" this.$route.params.id)
+      .then(response => {
+        this.$router.push("/");
+    });
+},
+  printableLetter: function() {
+    axios.
+      .get("/api/letters/" + this.$route.params.id)
+      .then(response => {
+        this.$router.push("/letters/" + this$route.params.id + "/printable");
+    });
+}
+},
+watch: {
+  $route: function() {
+    axios
+    .get("/api/letters/" + this.$route.params.id)
+    .then(response => {
+      this.letter = response.data;
+    });
+  }
+}
+};
+
